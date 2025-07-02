a8254cbad790989dd4417551562dc4361ad554b0
AlexD717 <alex717nd@gmail.com>
2025-07-01 21:45:25 -0700

    Prettier config

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Board Game Turn Timer Web App
+
 Web-based app that helps keep track and manage how much time each player spends thinking.
 
 _Created for Summer of Making, which is hosted by summer.hackclub.com_
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 const port = 3000
 const base = "/Board-Game-Turn-Timer/"
