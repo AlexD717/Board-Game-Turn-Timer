3856f9befe6e64324914496af4e3cfe7d7466d1f
AlexD717 <alex717nd@gmail.com>
2025-07-01 21:11:05 -0700

    Default Vite React Project

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const port = 3000
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: port,
+    open: true, // Automatically open the browser
+  },
+})
