a3ba5061385a582473964dcfd3a6967d38a20b41
AlexD717 <alex717nd@gmail.com>
2025-07-01 21:19:54 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const port = 3000
+const base = "/Board-Game-Turn-Timer/"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,5 @@ export default defineConfig({
     port: port,
     open: true, // Automatically open the browser
   },
+  base: base,
 })
